ee31d4acae882f5080a1fc82d9cd6310292bee7c
Edilson Barbosa <edams@bol.com.br>
2017-06-12 22:33:15 -0300

    foi criado o CRUD

--- index.php
+++ index.php
@@ -57,6 +57,98 @@ $app->get('/admin/logout', function() {
 	exit;
 
 });
+//ROTA -- Tela que vai listar todos os usuarios
+$app->get("/admin/users", function() {
+
+	User::verifyLogin();
+
+	$users = User::listAll();
+
+	$page = new PageAdmin();
+
+	$page->setTpl("users", array(
+		"users"=>$users
+	)); //Nome do template -->> users
+
+});
+//ROTA --Tela Create
+$app->get("/admin/users/create", function() {
+
+	User::verifyLogin();
+
+	$page = new PageAdmin();
+
+	$page->setTpl("users-create"); //Nome do template -->> users
+	
+
+});
+//ROTA -- Tela para excluir usuario do sistema
+$app->get("/admin/users/:iduser/delete", function($iduser) {
+
+	User::verifyLogin();
+
+	$user = new User();
+
+	$user->get((int)$iduser);
+
+	$user->delete();
+
+	header("Location: /admin/users");
+	exit;
+	
+});
+//ROTA --Tela Update
+$app->get("/admin/users/:iduser", function($iduser) {
+
+	User::verifyLogin();
+
+	$user = new User();
+
+	$user->get((int)$iduser);
+
+	$page = new PageAdmin();
+
+	$page->setTpl("users-update", array( 
+		"user"=>$user->getValues()
+	)); 
+
+});
+//ROTA -- Tela para salvar
+$app->post("/admin/users/create", function() {
+
+	User::verifyLogin();
+
+	$user = new User();
+
+	$_POST["inadmin"] = (isset($_POST["inadmin"]))?1:0;
+
+	$user->setData($_POST);
+
+	$user->save();
+
+	header("LACATION: /admin/users");
+	exit;
+
+});
+//ROTA -- Tela para salvar a edicao
+$app->post("/admin/users/:iduser", function($iduser) {
+
+	User::verifyLogin();
+
+	$user = new User();
+
+	$_POST["inadmin"] = (isset($_POST["inadmin"]))?1:0;
+
+	$user->get((int)$iduser);
+
+	$user->setData($_POST);
+
+	$user->update();
+
+	header("Location: /admin/users");
+	exit;
+	
+});
 
 $app->run();
 
